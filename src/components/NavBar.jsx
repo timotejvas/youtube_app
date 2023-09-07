9e1595241963360f5facb3b97cbb579c6230806e
Timotej V <timotejv@icloud.com>
2023-09-07 16:11:13 +0200

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 const NavBar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+    <Link to="/youtube_app" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar />
